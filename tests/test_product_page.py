1e6086dbc2dabbd9b7ef9037894647b3dbebbd48
ILUXONCHIK <iluxon4ik@hotmail.com>
2017-12-18 14:47:52 +0000

    test: add phone number request url test

--- tests/test_product_page.py
+++ tests/test_product_page.py
@@ -15,11 +15,11 @@ class ProductPageTestCase(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def _setup_product(self, product_file_path):
+    def _setup_product(self, product_file_path, url = 'https://iluxonchik.github.io/'):
         product = None
         with open(product_file_path, mode='rb') as f:
             content = f.read()
-            product = Product(content)
+            product = Product(content, url)
         return product
 
     def test_is_phone_number_present_TRUE(self):
@@ -41,9 +41,22 @@ class ProductPageTestCase(unittest.TestCase):
     def test_build_phone_number_request_url(self):
         # test building XMLHttpRequest url where the phone number will be
         # requested
-        self.skipTest('TBI')
         product_file_path = PRODUCTS_FILE_PATH_FMT.format('Y1')
-        product = self._setup_product(product_file_path)
-        expected_url = 'https://www.olx.pt/ajax/misc/contact/phone/AXuRv/?pt=d62b55bff0e896f432f5915da5cbb5759e757c2570f1f9ec7cc1e00b026e825e7c4585ec7b5b072cd8d343ce15fc9d7832073896fd1b9f66b0c428aa0f65e85d'
+        product = self._setup_product(product_file_path, 'https://www.olx.pt/anuncio/vendo-capa-apple-verde-iphone-6-6s-plus-IDAXuRv.html')
+
+        # First, let's make sure that the listing ID was parsed correctly
+        expected_listing_id = 'AXuRv'
+        obtained_listing_id = product._get_listing_id_from_url()
+        self.assertEqual(expected_listing_id, obtained_listing_id,
+                         'Wrong listing ID parsed')
+
+        # Now let's make sure that the phoneToken was parsed correctly
+        expected_phone_token = '7d6a8bf435f2fd90908ffc262c80a280574d0c73148eeac546a40b4356d5455189b558545392be7f4f216d668171334eda00f82ec577ef80d65a5542756858e5'
+        obtained_phone_token = product._get_phone_token()
+        self.assertEqual(expected_phone_token, obtained_phone_token,
+                         'Wrong phoneToken parsed')
+
+        # Finally, let's test that the final url was built correctly
+        expected_url = 'https://www.olx.pt/ajax/misc/contact/phone/AXuRv/?pt=7d6a8bf435f2fd90908ffc262c80a280574d0c73148eeac546a40b4356d5455189b558545392be7f4f216d668171334eda00f82ec577ef80d65a5542756858e5'
         obtained_url = product.get_phone_number_url()
         self.assertEqual(expected_url, obtained_url, 'Wrong phone num url parsed')
