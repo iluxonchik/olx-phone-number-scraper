72561bdfbf2f33c74b784af64ad60cd2ea9ddde0
ILUXONCHIK <iluxon4ik@hotmail.com>
2018-02-12 02:27:39 +0000

    bug: numbers are no longer returning zeroes after about 80 requests
    As suspected, this was due to the limiting of number of phone number requests per session, so
    restarting the session after a little while solves the problem.

--- olxscraper/utils.py
+++ olxscraper/utils.py
@@ -1,26 +1,45 @@
 import requests
 
-print('[INFO] Setting up session...')
+REQUESTS_PER_SESSION = 80
+CURR_SESSION_REQEUSTS = 0  # trigger session init on first run
 
-session = requests.Session()
+session = None
 
-# NOTE: referer header is required!
-headers = {
-    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36",
-    "Accept": "*/*",
-    "Accept-Encoding":"gzip, deflate, br",
-    "X-Requested-With": "XMLHttpRequest"
-}
+def init_session():
+    global session
 
-session.headers.update(headers)
+    session = requests.Session()
 
-# conenct to olx in order to get the cookies, which will be sent on
-# subsequent requests
-res = session.get('https://olx.pt')
+    # NOTE: referer header is required!
+    # NOTE: the only required cookeis are: PHPSESSID and pt
+    headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36",
+        "Accept": "*/*",
+        "Accept-Encoding":"gzip, deflate, br",
+        "X-Requested-With": "XMLHttpRequest"
+    }
+
+    session.headers.update(headers)
+
+    # conenct to olx in order to get the cookies, which will be sent on
+    # subsequent requests
+    res = session.get('https://olx.pt')
+
+init_session()
+
+def check_session_requests_limit():
+    global CURR_SESSION_REQEUSTS, REQUESTS_PER_SESSION
+
+    if CURR_SESSION_REQEUSTS >= REQUESTS_PER_SESSION:
+        print('[INFO] Resetting session...')
+        init_session()
+        CURR_SESSION_REQEUSTS = 0
+
+    CURR_SESSION_REQEUSTS += 1
 
 
 def get_html_from_url(url, referer_header="https://olx.pt/"):
-
+    global session
     session.headers.update({"Referer":referer_header})
     html = None
     # NOTE: dirty hack; needed for testing.
@@ -31,4 +50,5 @@ def get_html_from_url(url, referer_header="https://olx.pt/"):
     else:
         res = session.get(url)
         html = res.text
+    check_session_requests_limit()
     return html
